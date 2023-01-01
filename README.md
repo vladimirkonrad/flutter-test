b9a79f4310672d390cec906c316f9affbb38917c
Vladimir Konrad <vladimirkonrad@gmail.com>
2023-01-01 06:57:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter
+Flutter Codespaces Template
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  print('Hello, World!')
+}
